ef322099cd54be86ffc85a0fa2b3b425790d4163
Dharshan PILLAY <dpillay@e5r2p4.wethinkcode.co.za>
2017-09-06 10:16:56 +0200

    testing

--- libft_RT/sdl.c
+++ libft_RT/sdl.c
@@ -6,7 +6,7 @@
 /*   By: dpillay <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/06 10:14:24 by dpillay           #+#    #+#             */
-/*   Updated: 2017/09/06 10:14:50 by dpillay          ###   ########.fr       */
+/*   Updated: 2017/09/06 10:16:31 by dpillay          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@ SDL_Renderer	*init(char *title)
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_Window *win = SDL_CreateWindow(title, 100, 100, 640, 480, SDL_WINDOW_SHOWN);
 	SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
+	//SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
 	SDL_RenderClear(ren);
 	SDL_RenderPresent(ren);
 	return (ren);
